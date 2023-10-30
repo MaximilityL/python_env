2682551170e78e14db03d2090c9bf3dfcbb62d77
Maxim Libman <maxim.libman@gmail.com>
2023-10-30 20:02:10 +0200

    cleanup and capsulation

--- data_distributions_ws/src/data_transformations.py
+++ data_distributions_ws/src/data_transformations.py
@@ -38,6 +38,7 @@ def extract_data_arrays(data_dict, time_dict, data_topic1, data_topic2, referenc
     return pitch_interp, velocityx_interp, time_vector
 
 def plot_data(pitch_interp, velocityx_interp, time_vector):
+    # Create a new figure explicitly
     plt.figure(figsize=(12, 6))
     plt.plot(time_vector, pitch_interp, label='Pitch (Euler Angle)')
     plt.plot(time_vector, velocityx_interp, label='Linear Velocity (x)')
@@ -45,6 +46,7 @@ def plot_data(pitch_interp, velocityx_interp, time_vector):
     plt.ylabel('Value')
     plt.title('Pitch and Linear Velocity vs. Time')
     plt.legend()
+    plt.ioff()
     plt.show()
 
 def cut_data_to_time_frame(pitch_array, velocityx_array, time_vector, start_time, end_time):
@@ -57,8 +59,55 @@ def cut_data_to_time_frame(pitch_array, velocityx_array, time_vector, start_time
 
     return pitch_cut, velocityx_cut, time_cut
 
+def calculate_mean_and_std(data_array):
+    mean_value = np.mean(data_array)
+    std_value = np.std(data_array)
+    return mean_value, std_value
 
-def main():
+def normalize_to_standard_normal(data_array):
+    mean_value = np.mean(data_array)
+    std_value = np.std(data_array)
+    
+    # Calculate the z-score
+    z_score = (data_array - mean_value) / std_value
+    
+    return z_score
+
+def calculate_pearson_correlation(data1, data2):
+    # Calculate the means of both datasets
+    mean_data1 = np.mean(data1)
+    mean_data2 = np.mean(data2)
+
+    # Calculate the standard deviations of both datasets
+    std_data1 = np.std(data1)
+    std_data2 = np.std(data2)
+
+    # Calculate the covariance between data1 and data2
+    covariance = np.cov(data1, data2)[0, 1]
+
+    # Calculate Pearson's correlation coefficient
+    correlation_coefficient = covariance / (std_data1 * std_data2)
+
+    return correlation_coefficient
+
+def calculate_rmse(data1, data2):
+    # Ensure data1 and data2 have the same length
+    if len(data1) != len(data2):
+        raise ValueError("Data arrays must have the same length")
+
+    # Calculate the squared differences between corresponding elements
+    squared_diff = (data1 - data2) ** 2
+
+    # Calculate the mean of the squared differences
+    mean_squared_diff = np.mean(squared_diff)
+
+    # Calculate the square root of the mean squared difference (RMSE)
+    # rmse = np.sqrt(mean_squared_diff)
+    rmse = mean_squared_diff
+
+    return rmse
+
+def getInterpolatedPitchAndVelocityData():
     bag_file = '/home/maxim/Workspaces/python_env/data_distributions_ws/src/2023-10-30-11-53-38.bag'
     desired_topics = ['/mavros/euler', '/mavros/local_position/odom', '/mavros/local_position/pose']
     reference_topic = '/mavros/euler'
@@ -66,20 +115,39 @@ def main():
     data_topic2 = '/mavros/local_position/odom'
 
     data_dict, time_dict = extract_data_and_timestamps(bag_file, desired_topics)
-    
     pitch_interp, velocityx_interp, time_vector = extract_data_arrays(data_dict, time_dict, data_topic1, data_topic2, reference_topic)
+    return pitch_interp, velocityx_interp, time_vector
 
-    # Define the start and end times for cutting
-    start_time = 10.0  # Adjust this as needed
-    end_time = 20.0  # Adjust this as needed
-
+def cutAndCalcAndPrintMeanAndSTD(pitch_interp, velocityx_interp, time_vector, start_time, end_time):
     pitch_cut, velocityx_cut, time_cut = cut_data_to_time_frame(pitch_interp, velocityx_interp, time_vector, start_time, end_time)
+    
+    print("Pitch Mean:", calculate_mean_and_std(pitch_cut)[0])
+    print("Pitch Standard Deviation:", calculate_mean_and_std(pitch_cut)[1])
+    print("Velocityx Mean:", calculate_mean_and_std(velocityx_cut)[0])
+    print("Velocityx Standard Deviation:", calculate_mean_and_std(velocityx_cut)[1])
+    return pitch_cut, velocityx_cut, time_cut
 
+def normalizeAndPrintPitchAndVelocity(pitch_cut,velocityx_cut, time_cut):
+    pitch_normalized = normalize_to_standard_normal(pitch_cut)
+    velocityx_normalized = normalize_to_standard_normal(velocityx_cut)
+
+    print("RMSE between data1 and data2:", calculate_rmse(pitch_normalized, velocityx_normalized))
+    print("Pearson Correlation Criteria:", calculate_pearson_correlation(pitch_normalized, velocityx_normalized))
 
     plot_data(pitch_cut, velocityx_cut, time_cut)
+    plot_data(pitch_normalized, velocityx_normalized, time_cut)
 
+    return pitch_normalized, velocityx_normalized
 
+def main():
+    
+    pitch_interp, velocityx_interp, time_vector = getInterpolatedPitchAndVelocityData()
+    start_time = 33.0
+    end_time = start_time + 3
+    
+    pitch_cut, velocityx_cut, time_cut = cutAndCalcAndPrintMeanAndSTD(pitch_interp, velocityx_interp, time_vector, start_time, end_time)
 
+    normalizeAndPrintPitchAndVelocity(pitch_cut,velocityx_cut, time_cut)
 
 if __name__ == "__main__":
     main()
